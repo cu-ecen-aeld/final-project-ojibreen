c2008b28168cf05407fa4a9aedfe820aade7767a
omar <omar.jibreen@gmail.com>
2024-06-07 03:14:34 +0000

    Sprint 3: Updated client to add ac type, manufacturer, and route info

--- client/adsbclient.py
+++ client/adsbclient.py
@@ -1,4 +1,5 @@
 import pyModeS as pms
+import requests
 import threading
 from pyModeS.extra.tcpclient import TcpClient
 from datetime import datetime
@@ -11,6 +12,9 @@ class ADSBClient(TcpClient):
     table = Table()
     table.add_column("ICAO")
     table.add_column("Callsign")
+    table.add_column("AC Type")
+    table.add_column("AC Manufacturer")
+    table.add_column("Route")
     table.add_column("Velocity")
     table.add_column("Altitude")
 
@@ -26,19 +30,47 @@ class ADSBClient(TcpClient):
                 self.table = Table()
                 self.table.add_column("ICAO")
                 self.table.add_column("Callsign")
+                self.table.add_column("AC Type")
+                self.table.add_column("AC Manufacturer")
+                self.table.add_column("Route")
                 self.table.add_column("Velocity")
                 self.table.add_column("Heading")
                 self.table.add_column("Altitude")
                 del_keys = []
+                ac_types_add_queue = []
+                route_add_queue = []
                 for icao, vals in self.adsb_dict.items():
                     now = datetime.now()
 
+                    if(vals["ac_type"] == "--"):
+                        # Add the icao hex code to the list of aircraft types to fetch from api.
+                        ac_types_add_queue.append(icao);
+                    
+                    if(vals["call"] != "--" and vals["route"] == "--"):
+                        route_add_queue.append((icao, vals["call"]))
+                    
                     # If there has been no activity on this flight, skip and mark for deletion.
                     if(((now - vals["updated"]).total_seconds()) / 60 > 2):
                        del_keys.append(icao)
                        continue
+                    
                     if(("call" in vals) and ("velocity" in vals) and ("alt" in vals)):
-                        self.table.add_row(f"{icao}", f"{vals['call']}", f"{vals['velocity']}", f"{vals['hdg']}", f"{vals['alt']}")
+                        self.table.add_row(f"{icao}", f"{vals['call']}", f"{vals['ac_type']}", f"{vals['ac_manufacturer']}", f"{vals['route']}", f"{vals['velocity']}", f"{vals['hdg']}", f"{vals['alt']}")
+                
+                for icao in ac_types_add_queue:
+                    res = requests.get("https://hexdb.io/api/v1/aircraft/" + icao)
+                    if(res.status_code == 200):
+                        data = res.json()
+                        self.adsb_dict[icao]["ac_type"] = data["ICAOTypeCode"]
+                        self.adsb_dict[icao]["ac_manufacturer"] = data["Manufacturer"]
+
+                for icao, callsign in route_add_queue:
+                    res = requests.get("https://hexdb.io/api/v1/route/iata/" + callsign)
+                    if(res.status_code == 200):
+                        data = res.json()
+                        self.adsb_dict[icao]["route"] = data["route"]
+                    elif(res.status_code == 404):
+                        self.adsb_dict[icao]["route"] = "Unknown"
                 
                 for k in del_keys:
                     del self.adsb_dict[k]
@@ -57,8 +89,8 @@ class ADSBClient(TcpClient):
                 now = datetime.now()
 
                 if(icao not in self.adsb_dict):
-                    self.adsb_dict[icao] = {"updated" : now, "call": "--", "velocity" : "--", "hdg" : "--", "alt" : "--"}
-        
+                    self.adsb_dict[icao] = {"updated" : now, "call": "--", "ac_type":"--","ac_manufacturer":"--", "route":"--", "velocity" : "--", "hdg" : "--", "alt" : "--"}
+                    
                 if(tc in range(1,5)):
                     callsign = pms.adsb.callsign(msg)
                     callsign = callsign.replace("_", "")
@@ -75,8 +107,6 @@ class ADSBClient(TcpClient):
                     alt = pms.adsb.altitude(msg)
                     self.adsb_dict[icao]["alt"] = alt
                     self.adsb_dict[icao]["updated"] = now
-                #sh = pms.adsb.speed_heading(msg)     # Handles both surface & airborne messages
-                #avel = pms.adsb.airborne_velocity(msg)
 # run new client, change the host, port, and rawtype if needed
 client = ADSBClient(host='10.0.0.104', port=30002, rawtype='raw')
 client.run()
